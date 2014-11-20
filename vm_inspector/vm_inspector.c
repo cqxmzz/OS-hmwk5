39310042c851310ff418108912439581b9cf86fa
Qiming Chen <qc2195@columbia.edu>
2014-11-20 11:38:48 -0500

    test program updated to alloc pgd space

--- vm_inspector/vm_inspector.c
+++ vm_inspector/vm_inspector.c
@@ -9,6 +9,7 @@
 #include <sys/mman.h>
 
 #define PAGE_TABLE_SIZE (4*1024*1024) /* 4mb */
+#define PGD_SIZE (2048*4)
 
 static int pgnum2index(int num)
 {
@@ -27,13 +28,19 @@ static unsigned long * expose(int pid)
 	int fd = open("/dev/zero", O_RDONLY);
 	void *addr = mmap(NULL, PAGE_TABLE_SIZE * 2, PROT_READ,
 				 MAP_SHARED, fd, 0);
+	void *pgd_addr = mmap(NULL, PGD_SIZE * 2, PROT_READ,
+				 MAP_SHARED, fd, 0);
 	close(fd);
 	if (addr == MAP_FAILED) {
 		printf("Error: mmap");
 		return NULL;
 	}
-	
-	if (syscall(378, pid, 0, (unsigned long)addr) < 0) {
+	if (pgd_addr == MAP_FAILED) {
+		printf("Error: mmap");
+		return NULL;
+	}
+	if (syscall(378, pid, (unsigned long)pgd_addr,
+			(unsigned long)addr) < 0) {
 		printf("Error: expose_page_table syscall");
 		return NULL;
 	}
