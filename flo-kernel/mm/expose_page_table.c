cbf196efafb1f7edf537f73cdccdf0cdce1207f9
Qiming Chen <qc2195@columbia.edu>
2014-11-19 20:09:00 -0500

    implement SYSCALL, need more help functions

--- flo-kernel/mm/expose_page_table.c
+++ flo-kernel/mm/expose_page_table.c
@@ -10,6 +10,12 @@
 #include <linux/slab.h>
 #include <asm/io.h>
 
+
+
+
+
+
+
 /* Map a target process's page table into address space of the current process.
  *
  * After successfully completing this call, addr will contain the 
@@ -28,5 +34,78 @@ SYSCALL_DEFINE3(expose_page_table, pid_t __user, pid,
 				unsigned long __user, fake_pgd,
 				unsigned long __user, addr)
 {
+	/* Qiming Chen */
+	struct mm_struct *mm;
+	struct vm_area_struct *user_vma = NULL;
+	struct vm_area_struct *curr_vma;
+	struct expose_pg_addrs *pg_addrs;
+	struct task_struct *task;
+	int errno;
+
+	/* check valid */
+	if (pid == NULL || addr == NULL || fake_pgd == NULL)
+		return -EINVAL;
+	
+	/* self */
+	if (pid == -1)
+		task = current;
+		mm = current->mm;
+	else {
+		task = find_task_by_vpid(pid);
+		if (!task)
+			return -EINVAL;
+		mm = task->mm;
+	}
+	/* check mm exist */
+	if (!mm)
+		return -EINVAL;
+	pg_addrs = kmalloc(sizeof(struct expose_pg_addrs), GFP_KERNEL);
+	if (!pg_addrs)
+		return -EFAULT;
+
+	INIT_LIST_HEAD(&pg_addrs->list);
+	pg_addrs->pid = pid;
+
+	/* lock */
+	down_read(&(mm->mmap_sem));
+
+	/* check user address valid */
+	user_vma = check_user_vma_is_valid(mm, (unsigned long)address);
+	if (!user_vma) {
+		kfree(pg_addrs);
+		up_read(&(mm->mmap_sem));
+		return -EINVAL;
+	}
+	pg_addrs->address = address;
+
+	/* add the new pg_addrs to the list */
+	if (mm->pg_addrs)
+		list_add(&(pg_addrs->list), &(mm->pg_addrs->list));
+	else
+		mm->pg_addrs = pg_addrs;
+
+	/* get the list of VMAs */
+	curr_vma = mm->mmap;
+
+	/* go through the list of VMAs and copy the PTEs */
+	do {
+		errno = copy_ptes_from_vma(mm, curr_vma, user_vma, address);
+		if (errno < 0){
+			/*
+			if (mm->pg_addrs == pg_addrs)
+				mm->pg_addrs == NULL;
+			else
+				list_remove(pg_addrs->list);
+			kfree(pg_addrs);
+			*/
+			up_read(&(mm->mmap_sem));
+			return errno;
+		}
+		curr_vma = curr_vma->vm_next;
+	} while (curr_vma);
+
+	/* unlock */
+	up_read(&(mm->mmap_sem));
+
 	return 0;
 }
